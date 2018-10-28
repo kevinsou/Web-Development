90a628a21893db6fb2175667f5e2a783cd1b52cf
kevinsou <32343982+kevinsou@users.noreply.github.com>
2018-10-27 17:28:16 -0700

    Update script.js

--- projects/Background-Generator/script.js
+++ projects/Background-Generator/script.js
@@ -4,9 +4,11 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var ranBtn = document.querySelector(".randomBTN");
 var revBtn = document.querySelector(".reverseBTN");
+var switchBtn = document.querySelector(".switchBTN");
+var on = false;
 
-console.log(color1);
-console.log(color2);
+//console.log(color1);
+//console.log(color2);
 
 var rgbToHex = function(){
 	var hex = Number(Math.floor((Math.random()*255)+1)).toString(16);
@@ -24,8 +26,17 @@ var ranFullColorHex = function(){
 }
 
 function setGradient(){
-	body.style.background = "linear-gradient(to right, " + color1.value + "," +color2.value+")";
-	css.textContent = body.style.background + ";";
+	if(on===true){
+		switchBtn.style.background = "linear-gradient(to right, " + color1.value + "," +color2.value+")";
+		revBtn.style.background= "linear-gradient( " + color2.value + "," +color1.value+")";
+		body.style.background = "linear-gradient( " + color1.value + "," +color2.value+")";
+		css.textContent = body.style.background + ";";
+	} else{
+		switchBtn.style.background = "linear-gradient(to bottom, " + color1.value + "," +color2.value+")";
+		revBtn.style.background= "linear-gradient(to right, " + color2.value + "," +color1.value+")";
+		body.style.background = "linear-gradient(to right, " + color1.value + "," +color2.value+")";
+		css.textContent = body.style.background + ";";
+	}
 }
 
 function setReverse(){
@@ -44,11 +55,16 @@ function setRandom(event){
 	// color2.setAttribute("value" ,"#"+ranFullColorHex());
 	setGradient();
 }
-
-color1.addEventListener("input",setGradient);
-
-color2.addEventListener("input", setGradient);
-
+function switchClick(){
+	if(on === false){
+		on=true;
+	}else{
+		on=false;
+	}
+	setGradient();
+}
 ranBtn.addEventListener("click", setRandom);
-
 revBtn.addEventListener("click", setReverse);
+switchBtn.addEventListener("click", switchClick);
+color1.addEventListener("input",setGradient);
+color2.addEventListener("input", setGradient);
